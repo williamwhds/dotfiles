a87d90b097f550ac59fb93f000e3555f9c18f05a
William Oliveira <202211240022@ifba.edu.br>
2026-05-20 00:37:47 -0300

    wip: disko and btrfs configs

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,42 @@
     "flakes"
   ];
 
+  # save space by hardlinking identical files in the Nix store
+  nix.settings.auto-optimise-store = true;
+
+  # automatically clear NixOS generations older than 14 days
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  services.sanoid = {
+    enable = true;
+    interval = "daily";
+
+    datasets = {
+      "btrfs/root" = {
+        useTemplate = [ "7day-rolling" ];
+      };
+      "btrfs/home" = {
+        useTemplate = [ "7day-rolling" ];
+      };
+    };
+
+    templates = {
+      "7day-rolling" = {
+        daily = 7;        # keep exactly 7 days of snapshots
+        hourly = 0;
+        weekly = 0;
+        monthly = 0;
+        yearly = 0;
+        autoprune = true; # automatically deletes the 8th day snapshot
+        autosnap = true;  # automatically takes the new snapshot daily
+      };
+    };
+  };
+
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
