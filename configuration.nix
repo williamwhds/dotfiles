1aadb6664e752d5c9f358ee4946faa3459900171
William Oliveira <202211240022@ifba.edu.br>
2026-05-20 23:01:24 -0300

    chore: indent

--- configuration.nix
+++ configuration.nix
@@ -64,13 +64,13 @@
 
     templates = {
       "7day-rolling" = {
-        daily = 7;        # keep exactly 7 days of snapshots
+        daily = 7; # keep exactly 7 days of snapshots
         hourly = 0;
         weekly = 0;
         monthly = 0;
         yearly = 0;
         autoprune = true; # automatically deletes the 8th day snapshot
-        autosnap = true;  # automatically takes the new snapshot daily
+        autosnap = true; # automatically takes the new snapshot daily
       };
     };
   };
