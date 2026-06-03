40f879b013710eb80087934b52b48ce6c4cfdc85
William Oliveira <202211240022@ifba.edu.br>
2026-06-03 18:08:48 -0300

    feat: eden emulator v0.2.1

--- modules/nixos/packages/gaming.nix
+++ modules/nixos/packages/gaming.nix
@@ -1,5 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
+let
+  eden-emu = pkgs-unstable.eden.overrideAttrs (oldAttrs: {
+    version = "0.2.1";
+    src = pkgs-unstable.fetchFromGitea {
+      domain = "git.eden-emu.dev";
+      owner = "eden-emu";
+      repo = "eden";
+      tag = "v0.2.1";
+      hash = "sha256-79/JmIRWysoc3psJqMFyiNc2gjTY4VhJfdNaiTvisMk=";
+    };
+    patches = [ ];
+  });
+in
 {
   programs.steam = {
     enable = true;
@@ -17,5 +30,6 @@
 
   environment.systemPackages = with pkgs; [
     polychromatic
+    eden-emu
   ];
 }
