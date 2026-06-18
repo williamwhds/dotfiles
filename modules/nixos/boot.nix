f398ad8936a4bc1d04b2747578e273dd19675d6e
William Oliveira <202211240022@ifba.edu.br>
2026-06-18 08:43:19 -0300

    feat: latest kernel

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
