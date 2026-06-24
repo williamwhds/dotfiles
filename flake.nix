5d5509b972fbb2ca84f337ba6408f80209ba4350
William Oliveira <202211240022@ifba.edu.br>
2026-06-24 20:44:08 -0300

    chore: flake update; nixpkgs switch to release-26.05 for openblas fix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/release-26.05";
